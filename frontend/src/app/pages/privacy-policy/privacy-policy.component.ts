ed8f26ec8f129e9c9c5067ea8a4e8d67ec20fd1b
ManuAlonsoSec <hola@manualonsodj.es>
2026-02-18 16:03:23 +0100

    Legal pages created and text added

---
+++ frontend/src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './privacy-policy.component.html',
+})
+export class PrivacyPolicyComponent {}
